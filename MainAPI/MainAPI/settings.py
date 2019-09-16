84a66483f8b65c26b37385e7ff3a77382bb6b038
diego llanos <llanes1205@hotmail.com>
2019-09-16 17:55:09 -0400

    Forced 7

--- MainAPI/MainAPI/settings.py
+++ MainAPI/MainAPI/settings.py
@@ -49,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 ROOT_URLCONF = 'MainAPI.urls'
 
 TEMPLATES = [
