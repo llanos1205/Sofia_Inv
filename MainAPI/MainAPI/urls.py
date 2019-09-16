02f4a58920620d9e3dd7c190570f6a150c8fab6f
diego llanos <llanes1205@hotmail.com>
2019-09-16 18:05:29 -0400

    forced 8

--- MainAPI/MainAPI/urls.py
+++ MainAPI/MainAPI/urls.py
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from rest_framework import routers
-from MainAPI.Inventario import views
+from django.urls import path
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
