36bcdbad78da5e60fa3075a40c3814ba43176cf4
Diego Llanos <llanes1205@hotmail.com>
2019-09-19 10:12:06 -0400

    cambios sin git app

--- Sofia_Server/Sofia_App/urls.py
+++ Sofia_Server/Sofia_App/urls.py
@@ -7,5 +7,15 @@ urlpatterns = [
     # path('Equipos/<int:pk>/', views.Equipo_detail),
     path('Empresas/',views.EmpresaList.as_view()),
     path('Empresas/<int:pk>/',views.Empresa_Detail.as_view()),
+    path('Departamentos/',views.Departamento_List.as_view()),
+    path('Departamentos/<int:pk>/',views.Departamento_Detail.as_view()),
+    path('Areas/',views.Area_List.as_view()),
+    path('Areas/<int:pk>',views.Area_Detail.as_view()),
+    path('Regionales/',views.Regional_List.as_view()),
+    path('Regionales/<int:pk>/',views.Regional_Detail.as_view()),
+    path('Ubicaciones/',views.Ubicacion_List.as_view()),
+    path('Ubicaciones/<int:pk>/',views.Ubicacion_Detail.as_view()),
+    path('Gerencias/',views.Gerencia_List.as_view()),
+    path('Gerencias/<int:pk>/',views.Gerencia_Detail.as_view()),
 ]
 urlpatterns=format_suffix_patterns(urlpatterns)
