decac49061e9bee92e8b6d3c5d0420f2bae88032
Diego Llanos <llanes1205@hotmail.com>
2019-09-19 10:11:13 -0400

    cambios sin git app

--- Sofia_Server/Sofia_App/serializers.py
+++ Sofia_Server/Sofia_App/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from Sofia_Server.Sofia_App.models import Equipo,UsuarioAd,Empresa
+from Sofia_Server.Sofia_App.models import Equipo,UsuarioAd,Empresa,Area,Departamento,Ubicacion,Gerencia,Regional
 from rest_framework import serializers
 
 
@@ -17,11 +17,34 @@ class UsuarioAdSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=UsuarioAd
         fields=['idusuario_ad','nombre','apellido']
-class EquipoSerializer(serializers.HyperlinkedModelSerializer):
+class EquipoSerializer(serializers.ModelSerializer):
     class Meta:
         model=Equipo
         fields=['idequipo','marca','modelo','nro_serie','nro_activo_fijo','ip','ultima_observacion']
-class EmpresaSerializer(serializers.HyperlinkedModelSerializer):
+class EmpresaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Empresa
         fields=['idempresa','nombre','descripcion','estado']
+class DepartamentoSerializer(serializers.ModelSerializer):
+    areas=serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+    class Meta:
+        model=Departamento
+        fields=['iddepartamento','nombre','descripcion','estado','areas']
+class AreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Area
+        fields=['idarea','nombre','descripcion','departamento_iddepartamento','estado']
+
+class RegionalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Regional
+        fields=['idregional','nombre','estado']
+
+class UbicacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Ubicacion
+        fields=['idubicacion','direccion','edificio','estado']
+class GerenciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Gerencia
+        fields=['idgerencia','nombre','descripcion','estado']
