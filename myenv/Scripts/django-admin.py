9306093262be714ef1e82ad54a38a87cc0f8bd18
llanos1205 <llanes1205@hotmail.com>
2019-09-27 14:24:36 -0400

    asd

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\dell\source\repos\llanos1205\sofia_inv\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
