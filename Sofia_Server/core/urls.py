a78b1ae72ddd32502335af1237abbc9605c272f1
diego llanos <llanes1205@hotmail.com>
2019-10-04 20:04:05 -0400

    zc

--- Sofia_Server/core/urls.py
+++ Sofia_Server/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 from Sofia_Server.core import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
      path('hello/', views.HelloView.as_view(), name='hello'),
+     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
